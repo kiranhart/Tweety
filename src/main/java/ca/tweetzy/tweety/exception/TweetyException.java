a280238f7c6020bed81d87a7d79ae9b7300ca0f2
Kiran Hart <kiran.hart@live.com>
2022-04-04 00:42:44 -0400

    add missing conflict files for whatever reason got deleted
    Took 10 minutes

---
+++ src/main/java/ca/tweetzy/tweety/exception/TweetyException.java
@@ -0,0 +1,68 @@
+package ca.tweetzy.tweety.exception;
+
+import ca.tweetzy.tweety.debug.Debugger;
+import lombok.Getter;
+import lombok.Setter;
+
+public class TweetyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Should we save thrown exceptions to error.log file automatically when they are thrown?
+	 */
+	@Getter
+	@Setter
+	private static boolean errorSavedAutomatically = true;
+
+	/**
+	 * Create a new exception and logs it
+	 *
+	 * @param t
+	 */
+	public TweetyException(Throwable t) {
+		super(t);
+
+		if (errorSavedAutomatically)
+			Debugger.saveError(t);
+	}
+
+	/**
+	 * Create a new exception and logs it
+	 *
+	 * @param message
+	 */
+	public TweetyException(String message) {
+		super(message);
+
+		if (errorSavedAutomatically)
+			Debugger.saveError(this, message);
+	}
+
+	/**
+	 * Create a new exception and logs it
+	 *
+	 * @param message
+	 * @param t
+	 */
+	public TweetyException(Throwable t, String message) {
+		super(message, t);
+
+		if (errorSavedAutomatically)
+			Debugger.saveError(t, message);
+	}
+
+	/**
+	 * Create a new exception and logs it
+	 */
+	public TweetyException() {
+
+		if (errorSavedAutomatically)
+			Debugger.saveError(this);
+	}
+
+	@Override
+	public String getMessage() {
+		return "Report: " + super.getMessage();
+	}
+}
